1dbeb783149d4e9417da37b66f04281532a365fa
Alex Batko <alex@exitloop.ca>
2012-04-11 18:59:01 -0700

    added rstrip and tests

--- lib/rsubhak.rb
+++ lib/rsubhak.rb
@@ -39,4 +39,42 @@ class Rsubhak
       }
     end
   end
+
+  ##
+  # Recursively traverse a hash or array *ha* (potentially of other nested
+  # hashes and/or arrays), searching for all values that respond to *strip*,
+  # and calling *strip!* on them to remove leading and trailing whitespace.
+  #
+  # ==== Parameters
+  # *ha*:: hash or array
+  #
+  # ==== Examples
+  #   rstrip(params)
+  #
+  # ==== Pronunciation
+  # r strip
+  #
+  def self.rstrip(ha)
+    case ha
+    when Hash
+      ha.each { |key, value|
+        case value
+        when Hash
+          self.rstrip(ha[key])
+        when Array
+          self.rstrip(value)
+        else
+          value.strip! if value.respond_to?(:strip) # base case
+        end
+      }
+    when Array
+      ha.each_with_index { |item, index|
+        if item.class == Hash || item.class == Array
+          self.rstrip(item)
+        elsif item.respond_to?(:strip) # base case
+          ha[index].strip!
+        end
+      }
+    end
+  end
 end
